9cfb6ecd7107834dc7a0a220d6039f65eace8020
ali-shayesteh <ali.shayesteh@gmail.com>
2023-12-23 17:35:24 +0330

    add home table

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,34 +1,24 @@
 <script setup>
-import { ref } from 'vue'
-const texts = ref({
-  title: 'Implementation, comparison and performance analysis of Reactjs and Vuejs frameworks',
-  description:
-    'Comparing performance and implementation of web pages using modern JavaScript libraries by implementing the same pages including different web elements (sliders, charts, etc.) in reactjs and vuejs.',
-
-  date: 'December 2023',
-  university: 'Shiraz university, Faulty of e-learning',
-  professor: 'Professor koorush ziarati',
-  name: 'Ali Shayesteh Sadafian',
-  myTitle: 'Bachelor\'s student in software engineering'
-})
+import { DocumentIcon } from '@heroicons/vue/24/outline'
+import { TEXT_HOME as TEXT, HOME_TABLE } from '../constants/texts/home'
 </script>
 
 <template>
   <div class="flex-col md:flex-row flex gap-10 text-slate-700">
     <div class="basis-2/3">
       <h1 class="text-3xl w-5/6">
-        {{ texts.title }}
+        {{ TEXT.title }}
       </h1>
-      <p class="text-slate-400 text-sm my-4 w-5/6">{{ texts.description }}</p>
-      <div>{{ texts.date }}</div>
+      <p class="text-slate-400 text-sm my-4 w-5/6">{{ TEXT.description }}</p>
+      <div class="text-base">{{ TEXT.date }}</div>
       <div class="w-24 border-t border-slate-300 mt-2 mb-6" />
       <div class="flex gap-6 items-center">
         <div>
-          <img src='../assets/img/shiraz-university-logo.png' />
+          <img src="../assets/img/shiraz-university-logo.png" />
         </div>
         <div>
-          <h3 class="text-xl">{{ texts.university }}</h3>
-          <h3 class="text-xl">{{ texts.professor }}</h3>
+          <h3 class="text-xl">{{ TEXT.university }}</h3>
+          <h3 class="text-xl">{{ TEXT.professor }}</h3>
         </div>
       </div>
     </div>
@@ -40,10 +30,35 @@ const texts = ref({
       </div>
       <div>
         <div>
-          <h3 class="text-lg">{{ texts.name }}</h3>
-          <div class="text-sm text-slate-400">{{ texts.myTitle }}</div>
+          <h3 class="text-lg">{{ TEXT.name }}</h3>
+          <div class="text-sm text-slate-400">{{ TEXT.myTitle }}</div>
         </div>
       </div>
+    </div>
+  </div>
+  <div class="border border-slate-300 rounded-xl p-8 mt-8">
+    <h2 class="text-slate-500 text-lg flex gap-1 items-center">
+      <DocumentIcon class="w-5 h-5 text-slate-400" /> {{ TEXT.tableTitle }}
+    </h2>
+    <div class="mt-6 border rounded-lg overflow-hidden">
+      <table class="lib-table">
+        <tbody>
+          <tr class="bg-slate-50">
+            <td></td>
+            <td><img src="../assets/react.svg" class="w-8 h-8" /></td>
+            <td><img src="../assets/logo.svg" class="w-8 h-8" /></td>
+          </tr>
+          <tr v-for="row in HOME_TABLE" :key="row.heading">
+            <th>{{ row.heading }}</th>
+            <td v-for="item in row.values" :key="item.link">
+              <a target="_blank" :href="item.link" class="text-fuchsia-700 flex items-center gap-1"
+                >{{ item.name }}
+              </a>
+              <span class="text-sm text-slate-500">{{ item.npm }}</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
---
+++ src/constants/texts/home.js
@@ -0,0 +1,60 @@
+export const TEXT_HOME = {
+  title: 'Implementation, comparison and performance analysis of Reactjs and Vuejs frameworks',
+  description:
+    'Comparing performance and implementation of web pages using modern JavaScript libraries by implementing the same pages including different web elements (sliders, charts, etc.) in reactjs and vuejs.',
+
+  date: 'December 2023',
+  university: 'Shiraz university, faculty of e-learning',
+  professor: 'Professor koorush ziarati',
+  name: 'Ali Shayesteh Sadafian',
+  myTitle: "Bachelor's student in software engineering",
+  tableTitle: 'Libraries used'
+}
+
+export const HOME_TABLE = [
+  {
+    heading: 'framework',
+    values:[ { name: 'Reactjs', link: 'https://react.dev/', npm: 'react, react-dom v18.2.0' },
+    { name: 'Vuejs', link: 'https://vuejs.org/', npm: 'vue v3.3.4' } ]
+  },
+  {
+    heading: 'build tool',
+    values: [{ name: 'Vitejs', link: 'https://vitejs.dev', npm: '@vitejs/plugin-react v4.2.0' },
+     { name: 'Vitejs', link: 'https://vitejs.dev', npm: '@vitejs/plugin-vue v4.4.0' }]
+  },
+  {
+    heading: 'router',
+    values:[ {
+      name: 'React router dom',
+      link: 'https://reactrouter.com/',
+      npm: 'react-router-dom v6.21.0'
+    },
+    { name: 'Vue router', link: 'https://router.vuejs.org/', npm: 'vue-router v4.2.5' }]
+  },
+  {
+    heading: 'styling',
+    values: [{
+      name: 'tailwindcss',
+      npm: 'tailwindcss v3.4.0',
+      link: 'https://tailwindcss.com/'
+    },
+    {
+      name: 'tailwindcss',
+      npm: 'tailwindcss v3.4.0',
+      link: 'https://tailwindcss.com/'
+    }]
+  },
+  {
+    heading: 'icons',
+    values: [{
+      name: 'Heroicons',
+      npm: '@heroicons/react v2.1.1',
+      link: 'https://github.com/tailwindlabs/heroicons#react'
+    },
+    {
+      name: 'Heroicons',
+      npm: '@heroicons/vue v2.1.1',
+      link: 'https://github.com/tailwindlabs/heroicons#vue'
+    }]
+  }
+]
